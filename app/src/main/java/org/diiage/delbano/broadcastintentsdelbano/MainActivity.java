6474ec07eabf9bad2861c775f87f18e456e40230
Your Name <delbanobastien@gmail.com>
2018-04-24 18:36:11 +0200

    Initial commit

---
+++ app/src/main/java/org/diiage/delbano/broadcastintentsdelbano/MainActivity.java
@@ -0,0 +1,57 @@
+package org.diiage.delbano.broadcastintentsdelbano;
+
+import android.Manifest;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.os.BatteryManager;
+import android.support.v4.app.ActivityCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.telephony.SmsMessage;
+import android.util.Log;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class MainActivity extends AppCompatActivity {
+
+    private TextView batteryTxt;
+    private int batteryLevel;
+
+    private BroadcastReceiver mBatInfoReceiver = new BroadcastReceiver(){
+        @Override
+        public void onReceive(Context ctxt, Intent intent) {
+            batteryLevel = intent.getIntExtra(BatteryManager.EXTRA_LEVEL, 0);
+            batteryTxt.setText(String.valueOf(batteryLevel) + "%");
+            if (batteryLevel <= 85){
+                Toast.makeText(ctxt, "Y a plus de battery",
+                        Toast.LENGTH_LONG).show();
+            }
+        }
+    };
+
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        batteryTxt = (TextView) this.findViewById(R.id.txtBattery);
+        this.registerReceiver(this.mBatInfoReceiver, new IntentFilter(Intent.ACTION_BATTERY_CHANGED));
+
+    }
+
+    @Override
+    protected void onStart(){
+        ActivityCompat.requestPermissions(this,
+                new String[]{Manifest.permission.RECEIVE_SMS},
+                10);
+        super.onStart();
+    }
+}
---
+++ app/src/main/java/org/diiage/delbano/broadcastintentsdelbano/SmsReceiver.java
@@ -0,0 +1,38 @@
+package org.diiage.delbano.broadcastintentsdelbano;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.telephony.SmsMessage;
+import android.widget.Toast;
+
+/**
+ * Created by Bastien on 24/04/2018.
+ */
+
+public class SmsReceiver extends BroadcastReceiver {
+    @Override
+    public void onReceive(Context context, Intent intent) {
+
+                if (intent.getAction()
+                        .equals("android.provider.Telephony.SMS_RECEIVED")) {
+                    Bundle bundle = intent.getExtras();
+                    if (bundle != null) {
+                        Object[] pdus = (Object[]) bundle.get("pdus");
+                        if (pdus.length == 0) {
+                            return;
+                        }
+                        SmsMessage[] messages = new SmsMessage[pdus.length];
+                        StringBuilder sb = new StringBuilder();
+                        for (int i = 0; i < pdus.length; i++) {
+                            messages[i] = SmsMessage.createFromPdu((byte[]) pdus[i]);
+                            sb.append(messages[i].getMessageBody());
+                        }
+                        String sender = messages[0].getOriginatingAddress();
+                        String message = sb.toString();
+                        Toast.makeText(context, message, Toast.LENGTH_SHORT).show();
+                    }
+                }
+            }
+    }
